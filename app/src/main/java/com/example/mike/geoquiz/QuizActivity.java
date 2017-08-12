46ca83a35e9889ad083ad2f48d490baef0bbb4ed
mse139 <mse139@gmail.com>
2017-08-12 14:52:38 -0400

    exercise 1 complete

--- app/src/main/java/com/example/mike/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/mike/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.mike.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,16 +23,23 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,Toast.LENGTH_SHORT).show();
+               Toast t =  Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP,t.getXOffset(),t.getYOffset());
+                t.show();
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+
+
+                Toast t = Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP,t.getXOffset(),t.getYOffset());
+                t.show();
+
             }
         });
     }
